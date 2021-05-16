dfe3f9022aee5bbdeff1b4ac9bb77b6e0f2e0667
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-05-16 18:53:08 +0200

    implement isEmpty() and contains() in Interval

--- src/main/java/jessica/Calculator.java
+++ src/main/java/jessica/Calculator.java
@@ -2,8 +2,13 @@ package jessica;
 
 public class Calculator<T> {
 
-    public T sum(T x, T y) {
-        return x+y;
+    public <T extends Integer> Integer sum(T x, T y) {
+        return x.intValue()+y.intValue();
     }
-
+    public <T extends Double> Double sum(T x, T y) {
+        return x.doubleValue()+y.doubleValue();
+    }
+    public <T extends Float> Float sum(T x, T y) {
+        return x.floatValue()+y.floatValue();
+    }
 }
--- src/test/java/jessica/CalculatorTest.java
+++ src/test/java/jessica/CalculatorTest.java
@@ -6,10 +6,26 @@ import static org.hamcrest.Matchers.*;
 class CalculatorTest {
 
     @org.junit.jupiter.api.Test
-    void sum() {
+    void sumInteger() {
         Calculator<Integer> calc = new Calculator<>();
 
         Integer result = calc.sum(2,5);
         assertThat(result, is(7));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void sumDouble() {
+        Calculator<Double> calc = new Calculator<>();
+
+        Double result = calc.sum(2.4,5.5);
+        assertThat(result, is(7.9));
+    }
+    @org.junit.jupiter.api.Test
+    void sumFloat() {
+        Calculator<Float> calc = new Calculator<>();
+
+        Float result = calc.sum(2f,5f);
+        assertThat(result, is(7f));
     }
 }
